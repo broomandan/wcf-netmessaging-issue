d3cfee942a9a4cf8c627d81b5d08a0fd46fbf7c5
Robert Broomandan <rbroomandan@futuredontics.com>
2015-06-10 18:10:24 -0700

    Initial commit

---
+++ WCFServiceWithAzureServiceBusQueue.Host/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace WCFServiceWithAzureServiceBusQueue.Host
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Console.Title = "MyQueuedService Service";
+                Console.WriteLine("Ready to receive messages from Azure Service Bus queue.");
+
+
+                var serviceHost = new ServiceHost(typeof (MyQueuedService));
+
+                serviceHost.Open();
+
+                Console.WriteLine("\nPress [Enter] to close ServiceHost.");
+                Console.ReadLine();
+
+                serviceHost.Close();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception occurred: {0}", exception);
+                Console.WriteLine("\nPress [Enter] to exit.");
+                Console.ReadLine();
+            }
+            Console.WriteLine("\nPress [Enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WCFServiceWithAzureServiceBusQueue/MyQueuedService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace WCFServiceWithAzureServiceBusQueue
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class MyQueuedService : IMyQueuedService, IDisposable
+    {
+        public void DoAction1()
+        {
+            Console.WriteLine("Action1 performed!");
+        }
+
+        public void DoAction2(string message)
+        {
+            Console.WriteLine("Action2 performed! with message={0}", message);
+        }
+
+        public void DoAction3(Message message)
+        {
+            Console.WriteLine("MessageId={0}, MessageDescription={1}", message.Id, message.Description);
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Finished processing Message.");
+        }
+    }
+}
